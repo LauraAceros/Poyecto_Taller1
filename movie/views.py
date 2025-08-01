bb07271e210dee3f95b0d86d2bef80e4c0c73aae
LauraAceros <lsacerosm@eafit.edu.co>
2025-08-01 11:32:56 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 def home (request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render (request, 'home.html')
-    return render (request,'home.html',{'name':'Laura Sofia Aceros'})
+    return render (request,'home.html',{'name':'Laura Sofia Aceros Monsalve'})
 def about (request):
     return HttpResponse('<h2>Welcome to Page About<h2>')
