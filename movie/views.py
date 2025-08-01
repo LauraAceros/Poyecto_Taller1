ccf31b3519dfab48ed1a8e3ef5766cc21f76dd5e
LauraAceros <lsacerosm@eafit.edu.co>
2025-08-01 11:26:52 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home (request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render (request, 'home.html')
+    return render (request,'home.html',{'name':'Laura Sofia Aceros'})
+def about (request):
+    return HttpResponse('<h2>Welcome to Page About<h2>')
