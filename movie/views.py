f71eddb49cd00f6d1d356c3ac8f01d74019db425
LauraAceros <lsacerosm@eafit.edu.co>
2025-08-02 13:51:35 -0500

    búsqueda de películas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home (request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render (request, 'home.html')
-    return render (request,'home.html',{'name':'Laura Sofia Aceros Monsalve'})
+    #return render (request,'home.html',{'name':'Laura Sofia Aceros Monsalve'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies= Movie.objects.filter(title__icontains=searchTerm)
+    else:
+
+        movies = Movie.objects.all()
+    return render(request,'home.html',{'searchTerms':searchTerm, 'movies':movies})
+
+
 def about (request):
-    return HttpResponse('<h2>Welcome to Page About<h2>')
+    #return HttpResponse('<h2>Welcome to Page About<h2>')
+    return render (request,'about.html' )
